76b7ff7fa1f4f62987392ddb64925c26b91e2a79
Clément Hélardot <clement.helardot@accenture.com>
2022-01-04 17:54:23 +0100

    Classes in Typescript

---
+++ features/classes.ts
@@ -0,0 +1,32 @@
+class Vehicule {
+
+  constructor(public color: string) { }
+
+  protected honk(): void {
+    console.log('beep');
+  }
+}
+
+const vehicule = new Vehicule('orange');
+console.log(vehicule.color);
+
+
+class Car extends Vehicule {
+
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
+  private drive(): void {
+    console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car(4, 'red');
+
+car.startDrivingProcess();
