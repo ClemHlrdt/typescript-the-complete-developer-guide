5d42c24290f9ed55dbab572e5da45514f2fb20c4
ClemHlrdt <clement.helardot@gmail.com>
2022-02-13 19:50:46 +0100

    - Rename Sync to ApiSync - Created Model.ts to create a generic Model - Moved logic from User to Model to favor code reuse - Added new getAll method to Attributes.ts

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,23 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ name: 'new record', age: 0 });
-
-console.log(user.get('name'));
+const user = User.buildUser({ id: 1 });
 
 user.on('change', () => {
-  console.log('user was chaned');
+  console.log(user);
 });
 
-user.trigger('change');
-
-// Reminder on how 'this' works in Javascript
-
-// const colors = {
-//   color: 'red',
-//   printColor() {
-//     console.log(this.color);
-//   }
-// };
-
-// const printColor = colors.printColor;
-// printColor();
+user.fetch();
